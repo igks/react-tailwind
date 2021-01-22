30634f32036639c6ead4f2e9449eaa00b03d53a7
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2021-01-22 15:21:29 +0700

    add transition

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export const Button = () => {
   return (
     <>
-      <div className="bg-blue-300 p-2 rounded-lg text-white font-bold cursor-pointer hover:bg-blue-500  transform hover:scale-110 m-2">
+      <div className="bg-blue-300 p-2 rounded-lg text-white font-bold cursor-pointer hover:bg-blue-500  transform hover:scale-110 m-2 transition-all duration-500">
         Button 1
       </div>
 
-      <div className="border border-blue-300 p-2 rounded-full text-blue-300 font-bold hover:border-blue-500 hover:text-blue-500 transform hover:scale-110 m-2">
+      <div className="border border-blue-300 p-2 rounded-full text-blue-300 font-bold hover:border-blue-500 hover:text-blue-500 transform hover:scale-110 m-2 transition-all duration-500">
         Button 2
       </div>
     </>
