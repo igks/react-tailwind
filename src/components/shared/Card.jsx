4ff05acd365a75691cb152fe9fae8a5a052c8d93
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2021-01-22 14:52:06 +0700

    create card component

---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="shadow-lg p-4 rounded-2xl">
+      <div className="text-2xl font-bold font-sans text-gray-500 mb-2">
+        Title
+      </div>
+      <div className="">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+        reprehenderit dignissimos voluptate mollitia impedit ab quod suscipit,
+        deleniti molestias, natus eius libero fuga quae beatae dicta neque iure
+        commodi cum.
+      </div>
+    </div>
+  );
+};
+
+export default Card;
